e2f16bffb2c55f0761e5e7d3e9f6bf50dc1e6bc3
Võ Xuân Trí <16130629@st.hcmuaf.edu.vn>
2021-01-17 15:24:09 +0700

    'Update'

--- Mouse.js
+++ Mouse.js
@@ -1,8 +1,11 @@
-function Mouse(name){
-	this.name = name;
-	this.dead = false;
+function Mouse(name) {
+    this.name = name;
+    this.dead = false;
 }
-Mouse.prototype.die = function(){
-	this.dead = true;
+Mouse.prototype.die = function() {
+    this.dead = true;
+};
+Mouse.prototype.drink = function() {
+    console.log('Drinking');
 };
 module.exports = Mouse;
